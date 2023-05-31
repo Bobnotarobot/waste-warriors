c4f6675e78358fa5425e6b159287bee7da83fdc7
fa521 <fa521@ic.ac.uk>
2023-05-31 15:14:26 +0100

    Added basic event display, removed metadata? Might be bad but doesn't cause issues

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 
 export default function Home() {
+  const data = [{location: "Your house", time: "In 3 minutes"}, {location: "The Moon", time: "2078"}, { location:"Third example", time: "idk Tuesday"}]
   return (
     <div className={styles.container}>
       <Head>
@@ -16,6 +17,17 @@ export default function Home() {
 
       <main>
         <p>Click <Link href="/test/test">here</Link> to organise an event!</p>
+
+        <h3>Upcoming events:</h3>
+
+        <div className={styles.grid}>
+          {data.map(event => 
+            <div className={styles.eventcard}>
+              <h4>{event.location}</h4>
+              <p>{event.time}</p>
+            </div>
+          )}
+        </div>
       </main>
     </div>
   )
