3ae900fd60735021d59b45108653c4e7e1ed548c
Bobnotarobot <49870298+Bobnotarobot@users.noreply.github.com>
2023-05-31 22:20:19 +0100

    Add key when mapping events in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ events }) {
-  const data = [{ location: "Your house", time: "In 3 minutes" }, { location: "The Moon", time: "2078" }, { location: "Third example", time: "idk Tuesday" }]
+  // const data = [{ location: "Your house", time: "In 3 minutes" }, { location: "The Moon", time: "2078" }, { location: "Third example", time: "idk Tuesday" }]
   return (
     <div className={styles.container}>
       <Head>
@@ -30,9 +30,11 @@ export default function Home({ events }) {
 
         <div className={styles.grid}>
           {events.map(event =>
-            <div className={styles.eventcard}>
-              <h4>{event.location}</h4>
-              <p>{event.date}</p>
+            <div key={event.id}>
+              <div className={styles.eventcard}>
+                <h4>{event.location}</h4>
+                <p>{event.date}</p>
+              </div>
             </div>
           )}
         </div>
