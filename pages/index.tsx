890e0c09cb4d041328fb61bf2fd76beb1a921729
zaku0 <devilleneuveziya@gmail.com>
2023-06-07 03:11:15 +0100

    Added map view to home index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 'use client'
-import { useEffect } from 'react'
+import { useEffect, useState, useRef, MutableRefObject } from 'react'
 import Image from 'next/image'
 import styles from './page.module.css'
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import prisma from '../lib/prisma';
+import { useLoadScript, GoogleMap, MarkerF } from '@react-google-maps/api';
+import { useMemo } from 'react';
+import Geolocation from '@react-native-community/geolocation';
+const DEVELOPMENT_GOOGLE_MAPS_KEY="AIzaSyD_uZuWbXXwxHrP4jetAlgWzrrc-dgQ_6Q"
+const PRODUCTION_GOOGLE_MAPS_KEY="AIzaSyBXcHbmJFrRxrot8_NXQzNMBUITngrsWEo"
 
 export async function getServerSideProps() {
-  const events = await prisma.event.findMany();
+  //const events = await prisma.event.findMany();
+  const events = [{id:1, location:"51.5126, -0.1448", date:"2023-06-23"}, {id:2, location:"51.5226, -0.1348", date:"2023-06-09"}, {id:1, location:"51.5236, -0.1448", date:"2023-06-08"}, {id:4, location:"51.5136, -0.1448", date:"2023-06-06"}]
   return {
     props: { events }
   }
@@ -20,7 +26,93 @@ interface event {
   date: string;
 }
 
+interface marker {
+  id: number;
+  lat: number;
+  lng: number;
+  colour: string;
+  title: string;
+}
+
+const greenMarker = "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
+const yellowMarker = "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
+const redMarker = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+
+var markers: marker[] = [];
+
+export function generateMarkers(events: event[]) {
+
+  events.map((event: event) => {
+    const latLng = event.location.split(",").map(Number);
+
+    const eventDate = new Date(event.date).getTime();
+    const todayDate = new Date().getTime();
+
+    const oneDay = 24 * 60 * 60 * 1000;
+    const diffDays = Math.ceil((eventDate - todayDate) / oneDay);
+
+    var col;
+    var ttl;
+
+    if (diffDays == 1) {
+      col = redMarker;
+      ttl = "<1 day";
+    } else if (diffDays <= 3 && diffDays > 1) {
+      col = yellowMarker;
+      ttl = "<3 days";
+    } else if (diffDays > 3) {
+      col = greenMarker;
+      ttl = ">3 days";
+    } else {
+      // does not display stale markers ie. in the past wrt today
+      //TODO: remove stale events from database
+      return;
+    }
+    markers.push({id: event.id, lat: latLng[0], lng: latLng[1], colour: col, title: ttl});
+  });
+}
+
 export default function Home({ events }: any) {
+  const libraries = useMemo(() => ['places'], []);
+
+  var mapCenter = { lat: 51.5126, lng: -0.1448 };
+  /*if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+    mapCenter = { lat: position.coords.latitude, lng: position.coords.longitude };
+    });
+  }*/
+
+  const noMarkers = [
+    {
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [
+          { visibility: "off" }
+        ]
+      }
+    ];
+
+  const mapOptions = useMemo<google.maps.MapOptions>(
+    () => ({
+      disableDefaultUI: false,
+      clickableIcons: false,
+      scrollwheel: true,
+      styles: noMarkers
+    }),
+    []
+  );
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: DEVELOPMENT_GOOGLE_MAPS_KEY as string,
+    libraries: libraries as any,
+  });
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+
+  generateMarkers(events);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -29,21 +121,31 @@ export default function Home({ events }: any) {
       </Head>
 
       <main>
-        <p>Click <Link href="/organise">here</Link> to organise an event!</p>
-
-        <h3>Upcoming events:</h3>
-
-        <div className={styles.grid}>
-          {events?.map((event: event) =>
-            <div key={event.id}>
-              <div className={styles.eventcard}>
-                <h4>{event.location}</h4>
-                <p>{event.date}</p>
-              </div>
-            </div>
-          )}
-        </div>
+        <GoogleMap
+          options={mapOptions}
+          zoom={14}
+          center={mapCenter}
+          mapTypeId={google.maps.MapTypeId.ROADMAP}
+          mapContainerStyle={{ width: '800px', height: '800px' }}
+          onLoad={() => console.log('Map Component Loaded...')}
+        >
+          {markers.map((marker) => (
+              <MarkerF
+                key={marker.id}
+                position={{ lat: marker.lat, lng: marker.lng }}
+                icon={{
+                  url: marker.colour,
+                }}
+                title={marker.title}
+              />
+          ))};
+        </GoogleMap>
       </main>
     </div>
   )
 }
+
+//TODO: add info window to markers
+//TODO: link clicking marker to event page
+//TODO: find a way to hide maps api key in .env file
+//POTENTIAL TODO: add id number to marker to link to card (using filter tool)
