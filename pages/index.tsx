a1e01b7499c01de427b6657ead9cc1e1585ff682
zaku0 <devilleneuveziya@gmail.com>
2023-06-20 14:12:22 +0100

    Points for leaderboard take into account duration

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,14 +18,26 @@ import { useRouter } from 'next/router';
 import Header from './header';
 
 export async function getServerSideProps() {
-  const rawEvents = await prisma.event.findMany();
-  const events = rawEvents?.filter((event: any) => {
+  const rawEvents = await prisma.event.findMany({ include: { users: true } });
+  const events = rawEvents?.filter(async (event: any) => {
     const eventDate = new Date(event.date).getTime();
     const todayDate = new Date().getTime();
     if (eventDate >= todayDate) {
       return true;
     }
-    prisma.event.delete({ where: { id: event.id } });
+    const eventDuration = event.duration;
+    const eventUsers = event.users.map((user: any) => user.username);
+    const users = (await prisma.user.findMany({ 
+      include: {clan : true} 
+    })).filter((user: any) => eventUsers.includes(user.username) && user.clan);
+    
+    users.map(async (user: any) => {
+      const clan = user.clan;
+      await prisma.clan.update({
+        where: { name: clan.name },
+        data: { points: clan.points + eventDuration }});
+      });
+    await prisma.event.delete({ where: { id: event.id } });
     return false;
   });
   return {
